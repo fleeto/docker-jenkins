033072782a22b632ebdf9fb60567a48c5d773feb
Vincent Cui <dustise@gmail.com>
2016-09-24 15:00:30 +0800

    Update before   apt-transport-https

--- prepare.sh
+++ prepare.sh
@@ -15,13 +15,15 @@ fi
 # Ubuntu
 if [ "$SIG" = "U" ]; then
   export DEBIAN_FRONTEND="noninteractive"
+  apt-get update
+  apt-get -y upgrade
   apt-get install -y apt-transport-https ca-certificates
   apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
   echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
 
   apt-get update
   apt-get -y upgrade
-  apt-get install -y ca-certificates curl git subversion unzip\
+  apt-get install -y curl git subversion unzip\
   docker-engine \
   -y --no-install-recommends
 fi
