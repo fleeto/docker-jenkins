61dc6eab29afda20cfb1bbfcd3f68167625f48aa
Vincent Cui <github@fleeto.us>
2018-05-20 01:32:42 +0800

    Support for img/runc

--- run.sh
+++ run.sh
@@ -28,8 +28,8 @@ if [ $JENKINS_MODE = "MASTER" ]; then
     /usr/share/jenkins/jenkins.war
 fi
 if [ $JENKINS_MODE = "SLAVE" ]; then
-  MASTER_URL="-url $JENKINS_URL"
-	java -Duser.timezone=$TIMEZONE \
+  java -Duser.timezone=$TIMEZONE \
   -Dorg.jenkinsci.remoting.engine.JnlpProtocol3.disabled=true \
-  -cp /usr/share/jenkins/slave.jar hudson.remoting.jnlp.Main -headless $MASTER_URL "$@"
+  -cp /usr/share/jenkins/slave.jar hudson.remoting.jnlp.Main \
+  -headless -url $JENKINS_URL $JENKINS_SECRET $JENKINS_NAME
 fi
